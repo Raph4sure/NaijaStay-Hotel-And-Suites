ea5594f88032dfd8ad18c18dd9909b0a16fa49df
Raph <raph4sure007@gmail.com>
2025-04-20 01:53:54 +0100

    remove unused logo images and update UI components with new styles and navigation

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -9,10 +9,18 @@ const Img = styled.img`
   width: auto;
 `;
 
+const ImageName = styled.p`
+    color: var(--color-green-700);
+    font-size: 2rem;
+    font-weight: 500;
+    margin-top: -1rem;
+`;
+
 function Logo() {
   return (
     <StyledLogo>
       <Img src="/logo-light.png" alt="Logo" />
+      <ImageName>Naija&apos;Stay</ImageName>
     </StyledLogo>
   );
 }
