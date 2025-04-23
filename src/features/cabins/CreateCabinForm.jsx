36d792a85ef78b5a5c52414a36f158885a7734e0
Raph <raph4sure007@gmail.com>
2025-04-23 11:41:27 +0100

    refactor cabin addition process by implementing modal for CreateCabinForm and enhancing form submission handling

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -8,7 +8,7 @@ import { useForm } from "react-hook-form";
 import { useCreateCabin } from "./useCreateCabin";
 import { useEditCabin } from "./useEditCabin";
 
-function CreateCabinForm({ cabinToEdit = {} }) {
+function CreateCabinForm({ cabinToEdit = {}, onCloseModal }) {
     const { id: editId, ...editValues } = cabinToEdit;
     const isEditSession = Boolean(editId);
     const { createCabin, isCreating } = useCreateCabin();
@@ -34,6 +34,7 @@ function CreateCabinForm({ cabinToEdit = {} }) {
                 {
                     onSuccess: () => {
                         reset();
+                        onCloseModal?.();
                     },
                 }
             );
@@ -43,6 +44,7 @@ function CreateCabinForm({ cabinToEdit = {} }) {
                 {
                     onSuccess: () => {
                         reset();
+                        onCloseModal?.();
                     },
                 }
             );
@@ -54,7 +56,7 @@ function CreateCabinForm({ cabinToEdit = {} }) {
     }
 
     return (
-        <Form onSubmit={handleSubmit(onSubmit, onError)}>
+        <Form onSubmit={handleSubmit(onSubmit, onError)} type={onCloseModal ? 'modal' : 'regular'}>
             <FormRow label="Cabin name" error={errors?.name?.message}>
                 <Input
                     type="text"
@@ -142,7 +144,7 @@ function CreateCabinForm({ cabinToEdit = {} }) {
 
             <FormRow>
                 {/* type is an HTML attribute! */}
-                <Button variation="secondary" type="reset">
+                <Button variation="secondary" type="reset" onClick={()=> onCloseModal?.()}>
                     Cancel
                 </Button>
                 <Button disabled={isWorking}>
