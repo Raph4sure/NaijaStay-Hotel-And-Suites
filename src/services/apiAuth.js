e497e56a6aec3390370a6f90661d0a283bc8a966
Raph <raph4sure007@gmail.com>
2025-04-29 02:58:00 +0100

    add user signup functionality; implement useSignup hook and SignupForm; enhance user account management with UpdateUserDataForm and UpdatePasswordForm

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,4 +1,20 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
+
+export async function signup({ email, fullName, password }) {
+    const { data, error } = await supabase.auth.signUp({
+        email,
+        password,
+        options: {
+            data: {
+                fullName,
+                avatar: "",
+            },
+        },
+    });
+    if (error) throw new Error(error.message);
+
+    return data;
+}
 
 export async function login({ email, password }) {
     let { data, error } = await supabase.auth.signInWithPassword({
@@ -27,4 +43,34 @@ export async function logout() {
     const { error } = await supabase.auth.signOut();
 
     if (error) throw new Error(error.message);
+}
+
+export async function updateCurrentUser({ password, fullName, avatar }) {
+    let updateData;
+    if (password) updateData = { password };
+    if (fullName) updateData = { data: { fullName } };
+
+    const { data, error } = await supabase.auth.updateUser(updateData);
+
+    if (error) throw new Error(error.message);
+
+    if (!avatar) return data;
+
+    const fileName = `avatar-${data.user.id}-${Math.random()}`;
+
+    const { error: storageError } = await supabase.storage
+        .from("avatars")
+        .upload(fileName, avatar);
+
+           if (storageError) throw new Error(storageError.message);
+
+    const { data: updateUser, error: errorStorage } = await supabase.auth.updateUser({
+        data: {
+            avatar: `${supabaseUrl}/storage/v1/object/public/avatars//${fileName}`,
+        },
+    });
+
+    if (errorStorage) throw new Error(errorStorage.message);
+    
+    return updateUser;
 }
